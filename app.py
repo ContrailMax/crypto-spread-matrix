79a3cdd60c07bdcf63db28a704a8fb24d3257aa5
ContrailMax <contrailmax1@gmail.com>
2026-02-19 15:04:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ from google.cloud import bigquery
 
 # Set up the web page configuration
 st.set_page_config(layout="wide", page_title="Crypto Matrix")
-st.title("Crypto Arbitrage Matrix")
+st.title("🚀 Crypto Arbitrage Matrix")
 
 # Fetch data from BigQuery
 try:
-   # 1. Retrieve the service account key from Streamlit Secrets
+    # 1. Retrieve the service account key from Streamlit Secrets
     key_dict = json.loads(st.secrets["GCP_KEY"])
     credentials = service_account.Credentials.from_service_account_info(key_dict)
-    client = bigquery.Client(credentials=credentials, project=key_dict['project_id'])
+    client = bigquery.Client(credentials=credentials, project='project-2c68fafb-fc39-4b54-b6f')
     
     st.success("✅ Successfully connected to BigQuery!")
 
