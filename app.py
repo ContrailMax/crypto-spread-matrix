04ca38c4c7d67f185e767663e541e68002dbaa3d
ContrailMax <contrailmax1@gmail.com>
2026-02-19 13:49:14 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide", page_title="Crypto Matrix")
+st.title("🚀 Crypto Arbitrage Matrix")
+st.write("If you can see this message, the connection between GitHub and Streamlit is successful!")
+st.info("We will add the code to fetch data from BigQuery here in the next step.")
